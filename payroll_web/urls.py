413590a84cf793141bb3279b09c98fd3371d45f9
mohamad nurdiansyah <mohamad.nurdiansyah@mediawave.co.id>
2025-07-04 12:06:49 +0700

    using template django

--- payroll_web/urls.py
+++ payroll_web/urls.py
@@ -36,6 +36,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('main.urls')),
     path('api/', include('web.urls')),
 
     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
